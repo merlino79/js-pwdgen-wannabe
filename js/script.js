51760575d93e1cac0183511ba5d6e4e22af8d525
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-06 23:03:33 +0200

    prove none e cognome

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 var ilNome = prompt("come ti chiami");
+var ilCognome = prompt("il tuo cognome");
+var ilColore = prompt("il vs. colore preferito");
+
+var msgExit = "ciao" + ilNome + "" + ilCognome;
+
+document.getElementById('dati').innerHTML = msgExit;
