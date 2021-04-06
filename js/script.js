b090510bcf1febe954e3ce01f32903470dde47bd
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-06 23:08:52 +0200

    fine inserimento dati utente e colore scelta

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-var ilNome = prompt("come ti chiami");
-var ilCognome = prompt("il tuo cognome");
+var ilNome = prompt("il VS. nome");
+var ilCognome = prompt("il VS. cognome");
 var ilColore = prompt("il vs. colore preferito");
 
 var msgExit = "ciao " + ilNome + " " + ilCognome;
+var mesgColore = 'il tuo colore preferito é ' + "" + ilColore;
 
 document.getElementById('dati').innerHTML = msgExit;
+document.getElementById('color').innerHTML = mesgColore;
