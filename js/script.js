113dcde809f28086e3487c6779bead68ad5fe42f
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-06 23:05:35 +0200

    prova dati

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ var ilNome = prompt("come ti chiami");
 var ilCognome = prompt("il tuo cognome");
 var ilColore = prompt("il vs. colore preferito");
 
-var msgExit = "ciao" + ilNome + "" + ilCognome;
+var msgExit = "ciao " + ilNome + " " + ilCognome;
 
 document.getElementById('dati').innerHTML = msgExit;
