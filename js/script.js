8c62e971719494350f820c0387cff01ab8f8d96b
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-06 23:11:01 +0200

    commenti

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+// info e colore richiesta utente 
+
+
 var ilNome = prompt("il VS. nome");
 var ilCognome = prompt("il VS. cognome");
 var ilColore = prompt("il vs. colore preferito");
 
+
+
+
+
+
 var msgExit = "ciao " + ilNome + " " + ilCognome;
 var mesgColore = 'il tuo colore preferito é ' + "" + ilColore;
+
+// print a video 
 
 document.getElementById('dati').innerHTML = msgExit;
 document.getElementById('color').innerHTML = mesgColore;
